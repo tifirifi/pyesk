c413dba6ad943bceb874979592a5bfe6de1dda85
tifirifi <filiptabisz@outlook.com>
2017-04-16 17:45:12 +0200

    Register models in admin.py

--- pyesk/tasks/models.py
+++ pyesk/tasks/models.py
@@ -1,18 +1,28 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
 class Ticket(models.Model):
     PRIORITY_NAMES = (
-        ('niski', 'Niski'),
-        ('średni', 'Średni'),
-        ('wysoki', 'Wysoki'),
+        ('low', _('Low')),
+        ('medium', _('Medium')),
+        ('high', _('High')),
     )
 
-    user = models.ForeignKey("auth.User")
-    title = models.CharField(max_length=200)
-    code = models.CharField(max_length=10, null=True, blank=True)
-    category = models.ForeignKey("Category")
-    priority = models.CharField(max_length=40, choices=PRIORITY_NAMES)
+    user = models.ForeignKey('auth.User', verbose_name=_('User'))
+    title = models.CharField(max_length=200, verbose_name=_('Title'))
+    code = models.CharField(
+        max_length=10,
+        null=True,
+        blank=True,
+        verbose_name=_('Code'),
+    )
+    category = models.ForeignKey('Category')
+    priority = models.CharField(
+        max_length=40,
+        choices=PRIORITY_NAMES,
+        verbose_name=_('Priority'),
+    )
 
     def save(self, *args, **kwargs):
         if not self.code:
@@ -25,18 +35,7 @@ class Ticket(models.Model):
 
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.category_name
-
-
-class Priority(models.Model):
-    PRIORITY_NAMES = (
-        ('niski', 'Niski'),
-        ('średni', 'Średni'),
-        ('wysoki',  'Wysoki'),
-    )
+    name = models.CharField(max_length=100, verbose_name=_('Category'))
 
     def __str__(self):
         return self.name
